3169de87708f0e3368c85cc4541c23af3a12b858
iskaa02 <eaemk@protonmail.com>
2024-02-26 18:05:31 +0200

    change categorize user endpoint

--- admin.go
+++ admin.go
@@ -4,8 +4,10 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/iskaa02/sadeem-user-api/auth"
 	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
+	"github.com/lib/pq"
 )
 
 func registerAdminRoutes(g *echo.Group, db *sqlx.DB) {
@@ -16,10 +18,19 @@ func registerAdminRoutes(g *echo.Group, db *sqlx.DB) {
 		c.Bind(&u)
 		return updateUser(db, id, &u)
 	})
+	g.GET("/users/:id", func(c echo.Context) error {
+		id := ""
+		echo.PathParamsBinder(c).String("id", &id)
+		isAdmin, _ := c.Get(auth.IsAdminContextKey).(bool)
+		u, err := getUser(db, id, isAdmin)
+		if err != nil {
+			return err
+		}
+		return c.JSON(http.StatusOK, u)
+	})
 	g.POST("/category", func(c echo.Context) error {
 		data := Category{}
-		err := c.Bind(&data)
-		if err != nil {
+		if err := c.Bind(&data); err != nil {
 			return err
 		}
 		return addCategory(data.Name, data.Activated, db)
@@ -28,33 +39,43 @@ func registerAdminRoutes(g *echo.Group, db *sqlx.DB) {
 		data := Category{}
 		id := ""
 		echo.PathParamsBinder(c).String("id", &id)
-		err := c.Bind(&data)
-		if err != nil {
+		if err := c.Bind(&data); err != nil {
 			return err
 		}
 		return updateCategory(id, data.Name, data.Activated, db)
 	})
 	g.DELETE("/category/:id", func(c echo.Context) error {
-		// id := chi.URLParam(r, "id")
 		id := ""
 		echo.PathParamsBinder(c).String("id", &id)
 		return deleteCategory(id, db)
 	})
-	g.POST("/user/:id/categorize", func(c echo.Context) error {
+	g.POST("/users/:id/categorize", func(c echo.Context) error {
 		id := ""
 		echo.PathParamsBinder(c).String("id", &id)
 		data := map[string]string{}
 		if err := c.Bind(&data); err != nil {
 			return err
 		}
-		return categorizeUser(db, id, data["category_id"])
+		err := categorizeUser(db, id, data["category_id"])
+		if err != nil {
+			pgErr, ok := err.(*pq.Error)
+			if ok {
+				// if user already cateogrized do nothing
+				if pgErr.Code == "23505" {
+					return nil
+				}
+			}
+		}
+		return err
 	})
 
-	g.POST("/user/:id/uncategorize", func(c echo.Context) error {
+	g.POST("/users/:id/uncategorize", func(c echo.Context) error {
 		id := ""
 		echo.PathParamsBinder(c).String("id", &id)
 		data := map[string]string{}
-		c.Bind(&data)
+		if err := c.Bind(&data); err != nil {
+			return err
+		}
 		return uncategorizeUser(db, id, data["category_id"])
 	})
 
@@ -64,7 +85,8 @@ func registerAdminRoutes(g *echo.Group, db *sqlx.DB) {
 		if page < 0 {
 			page = 0
 		}
-		users, err := listUsers(db, page)
+		searchQuery := c.Request().URL.Query().Get("q")
+		users, err := listUsers(db, page, searchQuery)
 		if err != nil {
 			return err
 		}
